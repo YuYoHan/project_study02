f10dac3de07836246bafd8a4b8ee3a99cee8de5e
YuYoHan <zxzz8014@naver.com>
2022-11-19 15:13:00 +0900

    [Feature] SQL

---
+++ mayo_sql.sql
@@ -0,0 +1,45 @@
+create database mayo;
+use mayo;
+
+create table user (
+	userid varchar(300) primary key,
+    userpw varchar(300) not null,
+    username varchar(300) not null,
+    userphone varchar(300) unique,
+    useraddr varchar(1000) not null
+);
+
+create table myinfo(
+	usercode int primary key,
+    mypost varchar(300),
+    mylike varchar(300),
+    mycomment varchar(3000),
+    userid varchar(300),
+    constraint fk_userid foreign key(userid) references user(userid)
+);
+
+drop table myinfo;
+
+create table post(
+	postnumber int primary key auto_increment,
+    postname varchar(300) not null,
+    posttime date not null,
+     userid varchar(300),
+    constraint fk_userid2 foreign key(userid) references user(userid)
+);
+
+CREATE TABLE picture(
+    picnumber int primary key auto_increment,
+    piclike varchar(300),
+    piccomment varchar(300),
+    pictime date not null,
+    userid varchar(300),
+    constraint fk_userid3 foreign key(userid) references user(userid)
+);
+create table comment(
+     comment_number int primary key auto_increment,
+     comment_info varchar(3000),
+     comment_time date,
+     userid varchar(300),
+    constraint fk_userid4 foreign key(userid) references user(userid)
+);
